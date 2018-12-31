b07ecb337f5368b5d08db3382ff57537a2762614
gyson <eilian.yunsong@gmail.com>
2018-12-31 08:09:16 -0500

    Add more spec

--- lib/ane.ex
+++ lib/ane.ex
@@ -3,6 +3,18 @@ defmodule Ane do
   Documentation for Ane.
   """
 
+  @type atomics_ref() :: :atomics.atomics_ref()
+
+  @type tid() :: :ets.tid()
+
+  @type t_for_ane_mode() :: {tid(), atomics_ref(), atomics_ref(), map()}
+
+  @type t_for_ets_mode() :: {tid(), pos_integer()}
+
+  @type t() :: t_for_ane_mode() | t_for_ets_mode()
+
+  @spec new(pos_integer(), keyword()) :: t()
+
   def new(n, opts \\ []) do
     read = Keyword.get(opts, :read_concurrency, false)
     write = Keyword.get(opts, :write_concurrency, false)
@@ -30,26 +42,30 @@ defmodule Ane do
         a2 = :atomics.new(n, signed: false)
         e = :ets.new(:ane, table_opts)
 
-        {a1, a2, e, %{}}
+        {e, a1, a2, %{}}
 
       :ets ->
         e = :ets.new(:ets, table_opts)
 
-        {n, e}
+        {e, n}
     end
   end
 
-  defp lookup(a2, e, i, version) do
+  @spec lookup(tid(), atomics_ref(), non_neg_integer(), non_neg_integer()) :: any()
+
+  defp lookup(e, a2, i, version) do
     case :ets.lookup(e, [i, version]) do
       [{_, value}] ->
-        {version, value}
+        value
 
       [] ->
-        lookup(a2, e, i, :atomics.get(a2, i))
+        lookup(e, a2, i, :atomics.get(a2, i))
     end
   end
 
-  def get({a1, a2, e, cache} = ane, i) do
+  @spec get(t(), non_neg_integer()) :: {t(), any()}
+
+  def get({e, a1, a2, cache} = ane, i) do
     i = i + 1
 
     case :atomics.get(a2, i) do
@@ -64,13 +80,13 @@ defmodule Ane do
 
           # cache miss
           _ ->
-            {_, value} = updated = lookup(a2, e, i, version)
-            {{a1, a2, e, Map.put(cache, i, updated)}, value}
+            value = lookup(e, a2, i, version)
+            {{e, a1, a2, Map.put(cache, i, {version, value})}, value}
         end
     end
   end
 
-  def get({n, e} = ane, i) when is_integer(i) and i >= 0 and i < n do
+  def get({e, n} = ane, i) when is_integer(i) and i >= 0 and i < n do
     case :ets.lookup(e, i) do
       [{_, value}] ->
         {ane, value}
@@ -80,74 +96,86 @@ defmodule Ane do
     end
   end
 
-  defp commit(a2, e, i, expected, desired) do
+  @spec commit(tid(), atomics_ref(), non_neg_integer(), non_neg_integer(), non_neg_integer()) ::
+          :ok
+
+  defp commit(e, a2, i, expected, desired) do
     case :atomics.compare_exchange(a2, i, expected, desired) do
       :ok ->
         :ets.delete(e, [i, expected])
+        :ok
 
       actual when actual < desired ->
-        commit(a2, e, i, actual, desired)
+        commit(e, a2, i, actual, desired)
 
       _ ->
         :ets.delete(e, [i, desired])
+        :ok
     end
   end
 
-  def put({a1, a2, e, _} = ane, i, value) do
+  @spec put(t(), non_neg_integer(), any()) :: :ok
+
+  def put({e, a1, a2, _}, i, value) do
     i = i + 1
 
     new_version = :atomics.add_get(a1, i, 1)
 
     :ets.insert(e, {[i, new_version], value})
 
-    commit(a2, e, i, new_version - 1, new_version)
-
-    ane
+    commit(e, a2, i, new_version - 1, new_version)
   end
 
-  def put({n, e} = ane, i, value) when is_integer(i) and i >= 0 and i < n do
+  def put({e, n}, i, value) when is_integer(i) and i >= 0 and i < n do
     :ets.insert(e, {i, value})
-
-    ane
+    :ok
   end
 
-  defp clean_table(_, _, :"$end_of_table"), do: :ok
+  @spec clean_table(tid(), atomics_ref(), map(), any()) :: :ok
 
-  defp clean_table(e, current_versions, [i, version] = key) do
-    case current_versions do
-      %{^i => current_version} when version < current_version ->
-        :ets.delete(e, key)
+  defp clean_table(_, _, _, :"$end_of_table"), do: :ok
 
-      _ ->
-        :ok
+  defp clean_table(e, a2, cache, [i, version] = key) do
+    {updated_cache, current_version} =
+      case cache do
+        %{^i => v} ->
+          {cache, v}
+
+        _ ->
+          v = :atomics.get(a2, i)
+          {Map.put(cache, i, v), v}
+      end
+
+    if version < current_version do
+      :ets.delete(e, key)
     end
 
-    clean_table(e, current_versions, :ets.next(e, key))
+    clean_table(e, a2, updated_cache, :ets.next(e, key))
   end
 
-  def clean({_, a2, e, _} = ane) do
-    size = get_size(ane)
+  @spec clean(t()) :: :ok
 
-    current_versions =
-      1..size
-      |> Enum.map(fn i -> {i, :atomics.get(a2, i)} end)
-      |> Enum.into(%{})
-
+  def clean({e, _, a2, _}) do
     :ets.safe_fixtable(e, true)
-    clean_table(e, current_versions, :ets.first(e))
+    clean_table(e, a2, %{}, :ets.first(e))
     :ets.safe_fixtable(e, false)
-
     :ok
   end
 
   def clean({_, _}), do: :ok
 
+  @spec get_mode(t()) :: :ane | :ets
+
   def get_mode({_, _, _, _}), do: :ane
   def get_mode({_, _}), do: :ets
 
-  def get_size({_, a2, _, _}), do: :atomics.info(a2).size
-  def get_size({n, _}), do: n
+  @spec get_size(t()) :: pos_integer()
 
-  def get_table({_, _, e, _}), do: e
-  def get_table({_, e}), do: e
+  def get_size({_, _, a2, _}), do: :atomics.info(a2).size
+  def get_size({_, n}), do: n
+
+  @spec get_table(t()) :: :ets.tid()
+
+  def get_table({e, _, _, _}), do: e
+  def get_table({e, _}), do: e
 end
